c42f4922f85eaf6508440f026fcb54013e832dc8
Drake <dmossman@iastate.edu>
2016-03-13 17:57:50 -0500

    got updatePost.php working

--- my-app/updatePosts.php
+++ my-app/updatePosts.php
@@ -1,7 +1,19 @@
 <?php
 session_start();
-
+//echo "----------START OF UPDATPOSTS.PHP---------";
 //$_SESSION["username"];
-file_get_contents("posts.txt");
-$id = $_REQUEST["id"];
+//var_dump($_REQUEST)
+
+$posts = json_decode(file_get_contents("posts.txt"));
+ for($i = 0;$i<count($posts);$i++) {
+	if($posts[$i]->id == $_REQUEST["id"]){
+		$posts[$i]->message = $_REQUEST["message"];
+		file_put_contents("posts.txt",json_encode($posts));
+		echo file_get_contents("posts.txt");
+		return;
+	} else {
+		echo "failure";
+	}
+}
+//echo "-----------END OF UPDATPOSTS.PHP----------";
 ?>
