ce8a11adad11601c2de7bf244cc2f033759e69b4
Drake <dmossman@iastate.edu>
2016-03-13 23:38:54 -0500

    updatepost edited to include session username as author

--- my-app/updatePosts.php
+++ my-app/updatePosts.php
@@ -13,7 +13,13 @@ $posts = json_decode(file_get_contents("posts.txt"));
 		return;
 	}
 }
-$posts[] = $_REQUEST;
+
+$newPost = (object) array('title' => $_REQUEST["title"],
+						'message' => $_REQUEST["message"],
+						'author' => $_REQUEST["author"],
+						'time' => $_REQUEST["time"]);
+$newPost -> author = $_SESSION["username"];
+$posts[] = $newPost;
 file_put_contents("posts.txt",json_encode($posts));
 //echo "-----------END OF UPDATPOSTS.PHP----------";
 ?>
