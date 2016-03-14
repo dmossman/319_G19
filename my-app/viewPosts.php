b3641860a5a8926072d850471ab58f90af205cd8
Drake <dmossman@iastate.edu>
2016-03-13 19:54:46 -0500

    made changes mostly to updatePosts.php and editPost/makeEditDialogue functinos in login.html

--- my-app/viewPosts.php
+++ my-app/viewPosts.php
@@ -9,9 +9,9 @@ $data = json_decode($posts);
 $arr = array();
 $size = count($data);
 for($i=0;$i<$size; $i++) {
-	echo "<tr><td>".$data[$i]->title."<br>".$data[$i]->author."<br>".
-	$data[$i]->time."<br>".$data[$i]->message."<br>".
-	"<input id=\"button".$i."\" tag =\"updateButton\" type=\"button\" value=\"Update\"/><br><br></td></tr>";
+	echo "<tr><td><div>".$data[$i]->title."</div><br><div>".$data[$i]->author."</div><br><div>".
+	$data[$i]->time."</div><br><div>".$data[$i]->message."</div><br><div>".
+	"<input id=\"updateButton\" type=\"button\" value=\"Update\"/><br><br></td></tr>";
 }
 
 ?>
